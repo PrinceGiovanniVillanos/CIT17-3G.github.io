0034dae8c6894e0087252b62e696cd618b745625
pg2003 <113009509+pg2003@users.noreply.github.com>
2023-10-04 20:14:21 +0800

    Syntax

---
+++ Lesson1_php.php
@@ -0,0 +1,35 @@
+# This is a comment, and
+# This is the second line of the comment
+// This is a comment too. Each style comments only
+print "An example with single line comments";
+<html>
+
+/* This is a comment with multiline
+ Author : Mohammad Mohtashim
+ Purpose: Multiline Comments Demo
+ Subject: PHP
+*/
+print "An example with multi line comments";
+<body>
+    <a href="/lessons">Back</a>
+    <br />
+    <h1>Lesson 1</h1>
+    <?php
+    
+    echo "<h3>An example with single line comments</h3>";
+    echo "// This is a comment too. Each style comments only";
+
+   
+    echo "<br/>";
+    echo "<h3>An example with multi line comments</h3>";
+    echo "
+    /* <br/>
+    This is a comment with multiline<br/>
+    Author : Prince Giovanni Villanos<br/>
+    Purpose: First Lesson Demo<br/>
+    Subject: PHP<br/>
+    */";
+    ?>
+</body>
+
+</html>
---
+++ wild.bat
@@ -0,0 +1,8 @@
+@echo off
+git remote set-url origin https://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@github.com/CDisk12/CIT142G.git
+git pull
+pause
+git add .
+git commit -m "Auto upload"
+git push
+pause
