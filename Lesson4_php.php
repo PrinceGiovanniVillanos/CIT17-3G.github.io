090f98df32bf93019fce4d142c9154012188229e
pg2003 <113009509+pg2003@users.noreply.github.com>
2023-10-04 20:42:44 +0800

    Auto upload

---
+++ Lesson4_php.php
@@ -0,0 +1,74 @@
+<?php
+// Using 'if' without braces
+if (3 == 2 + 1)
+    echo "Great - My sanity is intact.<br>";
+
+// Using 'if' with braces
+if (3 == 2 + 1) {
+    echo "Great - My sanity is still";
+    echo " intact.<br>";
+}
+?>
+
+<html>
+
+<body>
+    <a href="/lessons">Back</a>
+    <br />
+    <h1>Lesson 4</h1>
+    <?php
+    echo "<h3>Braces and If statements</h3>";
+    echo "<h5>Without Braces</h5>";
+
+    // Without Braces
+    if (3 == 2 + 1)
+        echo "Great - My sanity is intact.<br>";
+
+    echo "<h5>With Braces</h5>";
+
+    // With Braces
+    if (3 == 2 + 1) {
+        echo "Great - My sanity is still ";
+        echo "intact.<br>";
+    }
+    ?>
+</body>
+
+</html>
+<?php
+// Using 'if' without braces
+if (3 == 2 + 1)
+    echo "Great - My sanity is intact.<br>";
+
+// Using 'if' with braces
+if (3 == 2 + 1) {
+    echo "Great - My sanity is still";
+    echo " intact.<br>";
+}
+?>
+
+<html>
+
+<body>
+    <a href="/lessons">Back</a>
+    <br />
+    <h1>Lesson 4</h1>
+    <?php
+    echo "<h3>Braces and If statements</h3>";
+    echo "<h5>Without Braces</h5>";
+
+    // Without Braces
+    if (3 == 2 + 1)
+        echo "Great - My sanity is intact.<br>";
+
+    echo "<h5>With Braces</h5>";
+
+    // With Braces
+    if (3 == 2 + 1) {
+        echo "Great - My sanity is still ";
+        echo "intact.<br>";
+    }
+    ?>
+</body>
+
+</html>
