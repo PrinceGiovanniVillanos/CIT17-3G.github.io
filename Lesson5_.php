5d72089675a22b7f333859c61d9d9c515d58ff82
pg2003 <113009509+pg2003@users.noreply.github.com>
2023-10-04 20:59:14 +0800

    Auto upload

---
+++ Lesson5_.php
@@ -0,0 +1,24 @@
+<?php
+// Display a greeting message
+echo "Hello, PHP on the command line!<br>";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Lesson 5</title>
+</head>
+
+<body>
+    <a href="/lessons">Back</a>
+    <br />
+    <h1>Lesson 5</h1>
+    <?php
+    echo "<h3>To run this file, use 'php test.php' in the command prompt</h3>";
+    // Display a greeting message
+    echo "Hello, PHP on the command line!<br>";
+    ?>
+</body>
+
+</html>
